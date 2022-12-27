bf30b5800f41a47e028c27f24941764f89ce3c0d
Ayman <ayhammohammed723@gmail.com>
2022-12-27 21:49:34 +0300

    all

---
+++ src/store/auth-context.js
@@ -0,0 +1,38 @@
+import react, { useState, useEffect } from "react";
+
+const AuthContext = react.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const loggedInInformation = localStorage.getItem("isLoggedIn");
+    if (loggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
